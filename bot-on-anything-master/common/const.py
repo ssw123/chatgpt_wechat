89630778b984978c96d5d7bad8d79be571a05ebe
ssw123 <1850334772@qq.com>
2023-03-29 10:06:32 +0800

    ww

---
+++ bot-on-anything-master/common/const.py
@@ -0,0 +1,16 @@
+# channel
+TERMINAL = "terminal"
+WECHAT = "wechat"
+WECHAT_MP = "wechat_mp"
+WECHAT_MP_SERVICE = "wechat_mp_service"
+QQ = "qq"
+GMAIL = "gmail"
+TELEGRAM = "telegram"
+SLACK = "slack"
+HTTP = "http"
+
+# model
+OPEN_AI = "openai"
+CHATGPT = "chatgpt"
+BAIDU = "baidu"
+BING = "bing"
